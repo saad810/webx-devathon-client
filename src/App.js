5d9c68a121df55573d3a482163fe9be2ee1cef0c
Ahmed Younas <142696963+4hmed7ounas@users.noreply.github.com>
2024-09-07 09:13:23 +0500

    add some changes to app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold bg-secondary-700">Helo World</h1>
+      <h1 className="text-2xl font-bold bg-secondary-700">Hello World</h1>
     </div>
   )
 }
